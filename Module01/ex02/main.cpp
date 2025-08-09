a23946f33380cf4995b65efcc65435575b754bf4
nas <nasim.dahman@outlook.com>
2025-08-09 14:37:20 +0200

    ex02

--- Module01/ex02/main.cpp
+++ Module01/ex02/main.cpp
@@ -18,9 +18,18 @@ int main()
 {
 	std::string string =  "HI THIS IS BRAIN";
 
-	char	*p = string;
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
 
-	// std::cout << string << std::endl;
+	std::cout << "The memory address of the string variable : " << &string << std::endl;
+	std::cout << "The memory address held by stringPTR : " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF : " << &stringREF << std::endl;
+
+	std::cout << "----------------------------------------------------------" << std::endl;
+
+	std::cout << "The value of the string variable : " << string << std::endl;
+	std::cout << "The value pointed to by stringPTR : " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF : " << stringREF << std::endl;
 
 	return (0);
 }
