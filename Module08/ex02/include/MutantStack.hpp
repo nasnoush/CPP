cb98a94b655713f00f76f4a2aa3f73e99194b040
Nasim Dahman <nadahman@c2r6s2.42lausanne.ch>
2025-10-28 12:42:13 +0100

    ok

--- Module08/ex02/include/MutantStack.hpp
+++ Module08/ex02/include/MutantStack.hpp
@@ -3,21 +3,74 @@
 
 #include <stack>
 #include <iostream>
+#include <vector>
 
 template <typename T> 
 class MutantStack : public std::stack<T> {
 
 	public :
+
+		typedef std::deque<T>::iterator it;
+
+		it begin();
+		it end();
+		// it begin() const;
+		// it end() const;
+
+
 		MutantStack();
 		MutantStack(const MutantStack& other);
 		MutantStack& operator=(const MutantStack& other);
 		~MutantStack();
 
-
-		 begin() ;
-		 end() ;
-
 	private :
+
 };
+
+template <typename T>
+MutantStack<T>::MutantStack()
+{
+
+}
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack& other)
+{
+	this->c = other.c;
+}
+template <typename T>
+MutantStack<T>& MutantStack<T>::operator=(const MutantStack& other)
+{
+	if (this != &other)
+		this->c = other.c;
+	return (*this);
+}
+
+template <typename T>
+MutantStack<T>::~MutantStack()
+{
+
+}
+template <typename T>
+MutantStack<T>::it begin()
+{
+	return (this->c.begin());
+}
+template <typename T>
+MutantStack<T>::it end()
+{
+	return (this->c.end());
+}
+
+// template <typename T>
+// it MutantStack<T>::begin() const
+// {
+// 	return (c.begin());
+// }
+// template <typename T>
+// it MutantStack<T>::end() const
+// {
+// 	return (c.end());
+// }
 
 #endif
--- Module08/ex02/src/main.cpp
+++ Module08/ex02/src/main.cpp
@@ -1,18 +1,36 @@
 #include "../include/MutantStack.hpp"
 
+// int main()
+// {
+// 	std::stack<int> stack;
+
+// 	stack.push(20);
+// 	stack.push(30);
+// 	stack.push(40);
+// 	stack.push(50);
+
+// 	std::cout << stack.size() << std::endl;
+
+// 	stack.pop();
+// 	stack.pop();
+
+// 	std::cout << stack.size() << std::endl;
+
+// }
+
 int main()
 {
-	std::stack<int> stack;
+	MutantStack<int> stack;
 
 	stack.push(20);
 	stack.push(30);
 	stack.push(40);
 	stack.push(50);
 
-	std::cout << stack.size() << std::endl;
 
-	stack.pop();
+	for (MutantStack<int>::it it = stack.begin(); it != stack.end(); ++it)
+		std::cout << *it << std::endl;
 
-	std::cout << stack.size() << std::endl;
+	return (0);
 
 }
